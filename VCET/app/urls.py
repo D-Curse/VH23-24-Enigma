88c0844ddfaec97c05abbde8d5c8ae3f42b0379a
D-Curse <dhruvbhatada@gmail.com>
2023-10-06 11:54:00 +0530

    login page

--- VCET/app/urls.py
+++ VCET/app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
